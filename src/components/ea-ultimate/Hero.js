5132c1dd88354ccd24bd72481666d0e7b0b1b0d7
Anton Zdanov <azdanov@pm.me>
2019-04-24 18:09:02 +0300

    feat(project): use styled for Hero

--- src/components/ea-ultimate/Hero.js
+++ src/components/ea-ultimate/Hero.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import tw from 'tailwind.macro'
+import styled from 'styled-components'
 import Logo from '../icons/Logo'
+import { Container } from '../styled'
 
 const Hero = () => (
-  <div className="gradient overflow-hidden">
-    <div className="container">
-      <div className="py-6 items-center flex-wrap flex">
-        <div className="ml-2 sm:ml-0 sm:w-2/3 text-white w-full">
-          <h1 className="font-semibold mb-3 sm:text-left text-4xl text-center">
-            Php Inspections (EA&nbsp;Ultimate)
-          </h1>
-          <p className="text-xl">
+  <Gradient>
+    <Container>
+      <Body>
+        <Content>
+          <Title>Php Inspections (EA&nbsp;Ultimate)</Title>
+          <Text>
             Based on{` `}
             <a
               href="https://github.com/kalessil/phpinspectionsea"
@@ -20,17 +21,45 @@ const Hero = () => (
             , focuses on finding defects, security, performance issues and,
             maintainability: 39 new and 20 enhanced inspections around bugs and security
             (and counting).
-          </p>
-          <p className="text-xl">From the creator of Php Inspections (EA Extended).</p>
-        </div>
-        <div className="sm:w-1/3 w-full">
-          <div className="mx-auto w-56">
+          </Text>
+          <Text>From the creator of Php Inspections (EA Extended).</Text>
+        </Content>
+        <LogoContainer>
+          <Wrapper>
             <Logo width="15rem" height="15rem" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+          </Wrapper>
+        </LogoContainer>
+      </Body>
+    </Container>
+  </Gradient>
 )
+
+const Gradient = styled.div`
+  background-color: #232d7c;
+  background-image: linear-gradient(
+    to bottom left,
+    rgb(25, 33, 108) 50%,
+    rgb(45, 58, 140) 50%
+  );
+  ${tw`overflow-hidden`}
+`
+const Body = styled.div`
+  ${tw`py-6 items-center flex-wrap flex`}
+`
+const Content = styled.div`
+  ${tw`ml-2 sm:ml-0 sm:w-2/3 text-white w-full`}
+`
+const Title = styled.h1`
+  ${tw`font-semibold mb-3 sm:text-left text-4xl text-center`}
+`
+const Text = styled.p`
+  ${tw`text-xl`}
+`
+const LogoContainer = styled.aside`
+  ${tw`sm:w-1/3 w-full`}
+`
+const Wrapper = styled.div`
+  ${tw`mx-auto w-56`}
+`
 
 export default Hero
--- src/components/styled.js
+++ src/components/styled.js
@@ -5,3 +5,25 @@ import tw from 'tailwind.macro'
 export const LightBlueLink = styled(Link)`
   ${tw`hover:underline text-light-blue-vivid-700`}
 `
+
+export const Container = styled.div`
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+
+  @media (min-width: 576px) {
+    max-width: 576px;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 768px;
+  }
+
+  @media (min-width: 992px) {
+    max-width: 992px;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 1200px;
+  }
+`
