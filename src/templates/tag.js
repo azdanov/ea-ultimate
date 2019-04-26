345c927b0ea5f8bb9ef599e23ff009726ee7ce1f
Anton Zdanov <azdanov@pm.me>
2019-04-26 06:36:14 +0300

    feat(tag): use styled

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -15,47 +15,65 @@ const Tag = ({ data: { allMarkdownRemark }, pageContext }) => {
     <Layout>
       <Seo title={tag} />
       <Container>
-        <div className="mx-5 md:mx-auto md:w-3/4 lg:w-2/3 xl:w-1/2">
-          <h3 className="text-center leading-tight text-3xl my-8">
+        <Wrapper>
+          <Title>
             Found{` `}
-            <span className="font-semibold">
+            <span className="found">
               {totalCount} post
               {totalCount === 1 ? `` : `s`}
             </span>
             {` `}
             tagged with "{tag}"
-          </h3>
+          </Title>
           <ul>
             {edges.map(({ node: post }) => (
-              <li key={post.fields.slug} className="shadow p-3 mb-3 bg-white rounded">
+              <ListSection key={post.fields.slug}>
                 <TitleLink to={post.fields.slug}>
                   <h2>{post.frontmatter.title}</h2>
                 </TitleLink>
-                <small className="block mt-1 mb-2">{post.frontmatter.date}</small>
+                <small className="date">{post.frontmatter.date}</small>
                 <p>{post.frontmatter.description}</p>
                 <LightBlueLink to={post.fields.slug}>View Post</LightBlueLink>
-              </li>
+              </ListSection>
             ))}
           </ul>
-          <footer className="flex justify-center my-6">
+          <Footer>
             <LightBlueLink to="/tags/">Browse all tags</LightBlueLink>
-          </footer>
-        </div>
+          </Footer>
+        </Wrapper>
       </Container>
     </Layout>
   )
 }
 
-Tag.propTypes = {
-  data: PropTypes.shape({}),
-  pageContext: PropTypes.shape({}),
-}
-
+const Wrapper = styled.div`
+  ${tw`mx-5 md:mx-auto md:w-3/4 lg:w-2/3 xl:w-1/2`}
+`
+const Title = styled.h1`
+  ${tw`text-center leading-tight text-3xl mt-8 mb-6`}
+  .found {
+    ${tw`font-semibold`}
+  }
+`
 const TitleLink = styled(Link)`
   h2 {
     ${tw`mb-0 text-xl leading-tight font-semibold`}
   }
 `
+const ListSection = styled.li`
+  ${tw`shadow p-3 mb-3 bg-white rounded`}
+  .date {
+    ${tw`block mt-1 mb-2`}
+  }
+`
+const Footer = styled.footer`
+  ${tw`flex justify-center my-6`}
+`
+
+Tag.propTypes = {
+  data: PropTypes.shape({}),
+  pageContext: PropTypes.shape({ tag: PropTypes.string }),
+}
 
 export default Tag
 
