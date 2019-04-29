dc7e4d2189e79e2312b79f9ee1bdc641995ad57c
Anton Zdanov <azdanov@pm.me>
2019-04-29 18:54:29 +0300

    feat(Reviews): use styled

--- src/components/ea-ultimate/Reviews.js
+++ src/components/ea-ultimate/Reviews.js
@@ -1,19 +1,30 @@
 import React from 'react'
+import tw from 'tailwind.macro'
+import styled from 'styled-components'
 
 const Reviews = () => (
-  <article className="py-6 my-6 bg-indigo-400">
-    <p className="flex flex-wrap items-baseline justify-center mb-0 sm:text-3xl text-2xl text-white tracking-tighter">
+  <Background>
+    <Content>
       Interested in what people say about us?{` `}
-      <a
+      <ButtonLink
         target="_blank"
         rel="noopener noreferrer"
         href="https://plugins.jetbrains.com/plugin/7622-php-inspections-ea-extended-/reviews"
-        className="bg-white font-semibold hover:bg-gray-100 md:my-0 ml-4 my-2 px-4 py-2 rounded shadow text-indigo-500 text-xl tracking-normal"
       >
         See&nbsp;Reviews
-      </a>
-    </p>
-  </article>
+      </ButtonLink>
+    </Content>
+  </Background>
 )
+
+const Background = styled.article`
+  ${tw`py-6 my-6 bg-indigo-400`}
+`
+const Content = styled.p`
+  ${tw`flex flex-wrap items-baseline justify-center mb-0 sm:text-3xl text-2xl text-white tracking-tighter`}
+`
+const ButtonLink = styled.a`
+  ${tw`bg-white font-semibold hover:bg-gray-200 md:my-0 ml-4 my-2 px-4 py-2 rounded shadow text-indigo-500 text-xl tracking-normal`}
+`
 
 export default Reviews
