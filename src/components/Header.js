9b9dbe6b3f4e0fb6f260d30ea4d3e22e5c59afe5
Anton Zdanov <azdanov@pm.me>
2019-04-29 19:17:41 +0300

    feat(Header): use styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,18 @@
 import { Link } from 'gatsby'
 import React from 'react'
 import PropTypes from 'prop-types'
+import tw from 'tailwind.macro'
+import styled from 'styled-components'
 import LogoTextless from './icons/LogoTextless'
 
 const Header = ({ title }) => (
-  <header className="py-6 container">
-    <nav className="flex items-center text-xl">
-      <Link
-        to="/"
-        className="flex font-bold items-center ml-2 sm:ml-0 text-2xl tracking-tight"
-      >
+  <StyledHeader>
+    <Nav>
+      <LinkLogo to="/">
         <LogoTextless className="mr-3" />
         {title}
-      </Link>
-      <div className="flex justify-around ml-auto w-56">
+      </LinkLogo>
+      <Links>
         <Link to="/" className="hover:underline" activeClassName="underline">
           Home
         </Link>
@@ -33,10 +32,23 @@ const Header = ({ title }) => (
         >
           Blog
         </Link>
-      </div>
-    </nav>
-  </header>
+      </Links>
+    </Nav>
+  </StyledHeader>
 )
+
+const StyledHeader = styled.header`
+  ${tw`py-6 container`}
+`
+const Nav = styled.nav`
+  ${tw`flex items-center text-xl`}
+`
+const LinkLogo = styled(Link)`
+  ${tw`flex font-bold items-center ml-2 sm:ml-0 text-2xl tracking-tight`}
+`
+const Links = styled.section`
+  ${tw`flex justify-around ml-auto w-56`}
+`
 
 Header.propTypes = {
   title: PropTypes.string,
